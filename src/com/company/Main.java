35b46e1a4d63257d5fef81e5c1e39cb194c8428b
askar.khaimuldin <askar.khaimuldin@astanait.edu.kz>
2021-01-26 23:31:27 +0600

    Another commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import com.company.interfaces.IAthlete;
+import com.company.interfaces.ILuxuryCar;
 import com.company.interfaces.IVehicle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -12,15 +15,57 @@ public class Main {
         cars.add(new UsualCar(50));
         cars.add(new TeslaModelS(700));
 
+
+        moveCars(cars);
+
+        stopCars(cars);
+
+        // Example 1
+        /* Uncomment this if you need the first example
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(new Cat("Thomas"));
+        animals.add(new Dog("Rex"));
+        animals.add(new Fox("Firefox"));
+        // It is not possible to instantiate an
+        // object using class Animal
+        // animals.add(new Animal("Unknown animal"));
+        for (Animal a : animals) {
+            System.out.print(a.getName() + " says ");
+            a.makeSound();
+        }
+        */
+
+        //Example 2
+        /* Uncomment this if you need the second example
+        IAthlete athlete = new JumpingSwimmer("James");
+
+        athlete.compete();
+        athlete.sayHooray();
+         */
+
+    }
+
+    public static void moveCars(List<IVehicle> cars) {
         for (IVehicle car : cars) {
             car.move();
+            if (car instanceof ILuxuryCar) {
+                ILuxuryCar c = (ILuxuryCar) car;
+                c.autoPilot();
+                c.turnOnConditioner();
+            }
         }
+    }
 
+    public static void stopCars(List<IVehicle> cars) {
         System.out.println("--------------------");
         for (IVehicle car : cars) {
             car.stop();
-        }
 
+            if (car instanceof ILuxuryCar) {
+                ILuxuryCar c = (ILuxuryCar) car;
+                c.turnOffConditioner();
+            }
+        }
     }
 
 
--- src/com/company/interfaces/IVehicle.java
+++ src/com/company/interfaces/IVehicle.java
@@ -1,6 +1,8 @@
 package com.company.interfaces;
 
 public interface IVehicle {
+    int v = 54;
+
     void move();
     void stop();
 }
